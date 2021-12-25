5076bfec8ad31529cafc228e6421116e0159ee71
prom12 <51293420+Prom12@users.noreply.github.com>
2021-12-25 14:11:42 +0000

    update most features and functionalities

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
-      count: 0,
+      count: 1,
       cart: 0,
       nav_tab: false,
+      cart_tab: true,
+      products: [],
     };
   },
   methods: {
+    CartAdd(Product) {
+      var value = this.products.some((product) => {
+        return Product.name == product.name;
+      });
+      console.log(value);
+      if (value) {
+        this.count++;
+      } else {
+        this.products.unshift(Product);
+        this.cart++;
+      }
+    },
+    CartDelete(Product) {
+      this.products.splice(Product, 1);
+    },
     increment() {
       this.count += 1;
     },
@@ -20,11 +37,15 @@ const app = Vue.createApp({
     handleView() {
       this.nav_tab = window.innerWidth >= 600;
     },
+    toggle_cart() {
+      this.cart_tab = !this.cart_tab;
+    },
   },
 
   created() {
     this.handleView();
     window.addEventListener("resize", this.handleView);
+    this.products = [{ id: 1, name: "Fall Limit", amount: 125, quantity: 1 }];
   },
 });
 
